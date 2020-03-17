d92e4a4be8680b002a7e93f92c590ceba5ea386a
Jonas Schmid <jonas.schmid@gmail.com>
2020-03-17 17:37:32 +0100

    Added View binding

--- app/src/main/java/pro/schmid/viewbindinglint/MainActivity.kt
+++ app/src/main/java/pro/schmid/viewbindinglint/MainActivity.kt
@@ -2,11 +2,14 @@ package pro.schmid.viewbindinglint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pro.schmid.viewbindinglint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = "Hello from Activity"
     }
 }
